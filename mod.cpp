06d9ff76c642900073bb5e887d01ddf1871d935d
Steve Zhao <ampersand38@gmail.com>
2023-03-12 22:54:00 -0400

    url

--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,7 @@
 name = "Military Tandem Tethered Bundle";
 picture = "logo_mttb_ca.paa";
 actionName = "GitHub";
-action = "https://github.com/ampersand38/ride-where-you-look";
+action = "https://github.com/ampersand38/military-tandem-tethered-bundle";
 description = "Tandem freefall with a tethered cargo bundle.";
 logo = "logo_mttb_ca.paa";
 logoOver = "logo_mttb_ca.paa";
